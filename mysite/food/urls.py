4b7366dd86d465bd5bb88f8b76e92c8c92d69350
AyushRatan <ayushratan.974@gmail.com>
2023-01-11 17:35:42 +0530

    added many features

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 
 app_name = "food"
 urlpatterns = [
-    path("",views.index,name="index"),
+    path("",views.IndexClassView.as_view(),name="index"),
     path("item/",views.item,name="item"),
-    path("<int:item_id>/",views.detail,name="detail"),
+    path("<int:pk>/",views.FoodDetail.as_view(),name="detail"),
     # add items
     path("add/",views.create_item,name='create_item'),
     #edit items
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -2,29 +2,42 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Item
 from .forms import ItemForm
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from django.template import loader
 
 # Create your views here.
-def index(request):
+'''def index(request):
     item_list = Item.objects.all()
     #template = loader.get_template("food/index.html")
     context = {
         "item_list" : item_list,
     }
     #return HttpResponse(template.render(context,request))
-    return render(request,"food/index.html",context)
+    return render(request,"food/index.html",context)'''
+
+class IndexClassView(ListView):
+    model = Item
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'
+
 
 
 def item(request):
     return HttpResponse("<h1>This is an item view</h1>")
 
 
-def detail(request,item_id):
+'''def detail(request,item_id):
     item = Item.objects.get(pk=item_id)
     context = {
         "item":item,
     }
-    return render(request,"food/detail.html",context)
+    return render(request,"food/detail.html",context)'''
+
+class FoodDetail(DetailView):
+    model = Item
+    template_name = "food/detail.html"
 
 def create_item(request):
     form = ItemForm(request.POST or None)
@@ -37,6 +50,10 @@ def create_item(request):
         return redirect('food:index')
 
     return render(request,'food/item-form.html',context)
+
+# class based create view
+
+class CreateI
 
 
 def update_item(request,id):
